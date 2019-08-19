5f80084be835428ec243dd3b0114ee7d492165bd
guisopo <solerguillermo@hotmail.com>
2019-08-20 01:46:16 +0200

    Create header.php and add basic doctype

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Folio
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div id="page" class="site">
