8b836f3b2dd4686c5c641fc983b4de5875a52301
guisopo <solerguillermo@hotmail.com>
2019-08-20 03:07:57 +0200

    Clean up

--- header.php
+++ header.php
@@ -14,10 +14,12 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+  <h1><?php bloginfo( 'name' ) ?></h1>
+
   <div id="page" class="site">
