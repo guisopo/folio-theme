2a4bbab7d39c94acf7cfd0bc333b568213f39be8
guisopo <solerguillermo@hotmail.com>
2019-08-22 20:00:59 +0200

    Require taxonomy file

--- functions.php
+++ functions.php
@@ -10,5 +10,6 @@
 require get_template_directory().'/inc/cleanup.php';
 require get_template_directory().'/inc/enqueue.php';
 require get_template_directory().'/inc/theme-support.php';
+require get_template_directory().'/inc/works-taxonomies.php';
 require get_template_directory().'/inc/works-cpt.php';
 require get_template_directory().'/inc/works-cf.php';
