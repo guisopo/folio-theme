18c9545cde417e5490d66d5b0774ea84756756de
guisopo <solerguillermo@hotmail.com>
2019-08-23 18:55:24 +0200

    Clean up

--- template-parts/cf-work-details.php
+++ template-parts/cf-work-details.php
@@ -91,8 +91,9 @@
       rows="1"
       cols="20"
       placeholder="Director, camera, performance, editing..."
-    ><?php echo esc_attr( $work_credits ); ?>
-    </textarea>
+    ><?php 
+      echo esc_attr( $work_credits ); 
+    ?></textarea>
   </p>
   <!-- Work Duration -->
   <p>
@@ -134,6 +135,7 @@
       name="folio_work[description]"
       rows="5"
       cols="50"
-    ><?php echo esc_attr( $work_description ); ?>
-    </textarea>
+    ><?php 
+      echo esc_attr( $work_description ); 
+    ?></textarea>
   </p>
