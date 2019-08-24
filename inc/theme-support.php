45433f17bf83261c76d852dd72fdddf12d74b4da
guisopo <solerguillermo@hotmail.com>
2019-08-24 10:19:19 +0200

    Clean up

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -8,38 +8,13 @@
  */
 
 function folio_theme_setup() {
+  $post_formats = array( 'gallery', 'image', 'video' );
+  $html5 = array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' );
 
   add_theme_support( 'title-tag' );
-
-  /**
-   * Enable support for post thumbnails and featured images
-   * https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-   */
-
   add_theme_support( 'post-thumbnails' );
-
-  /**
-   * Enable support for the following post formats
-   * https://developer.wordpress.org/themes/functionality/post-formats/
-   */
-
-  add_theme_support( 'post-formats', array( 
-  'gallery', 
-  'image', 
-  'video' 
-  ) );
-
-  /**
-   * Enable support to HTML5 semantic markup
-   */
-
-  add_theme_support( 'html5', array(
-    'search-form',
-    'comment-form',
-    'comment-list',
-    'gallery',
-    'caption',
-  ) );     
+  add_theme_support( 'post-formats', $post_formats);
+  add_theme_support( 'html5', $html5 );     
 }
 
 add_action( 'after_setup_theme', 'folio_theme_setup' );
