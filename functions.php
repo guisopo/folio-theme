3d899e24463a44fed8b0581713fbf9093f3b3dd7
guisopo <solerguillermo@hotmail.com>
2019-08-28 16:23:35 +0200

    Delete and move CPT to Avant-Folio plugin

--- functions.php
+++ functions.php
@@ -9,5 +9,4 @@
 
 require get_template_directory().'/inc/cleanup.php';
 require get_template_directory().'/inc/enqueue.php';require get_template_directory().'/inc/works-taxonomies.php';
-require get_template_directory().'/inc/works-cpt.php';
 require get_template_directory().'/inc/works-cf.php';
