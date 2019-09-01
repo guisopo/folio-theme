b866ce2bc45cc2f368291232ede7eb950fd59dec
guisopo <solerguillermo@hotmail.com>
2019-09-01 10:24:18 +0200

    Add classes and remove h2

--- front-page.php
+++ front-page.php
@@ -13,22 +13,25 @@ get_header();
 <div id="primary" class="content-area">
   <main id="main">
 
-  <?php
-    $terms = get_terms( 'work_type' );
+    <ul class="work_types">
+      <?php
+        $terms = get_terms( 'work_type' );
+        $term_class = 'work_type';
 
-    foreach ( $terms as $term ) {
+        foreach ( $terms as $term ) {
 
-      $term_link = esc_url( get_term_link( $term->name, $term->taxonomy ) );
+          $term_link = esc_url( get_term_link( $term->name, $term->taxonomy ) );
 
-      echo (
-        '<h2>
-          <a href="' . $term_link  . '">' 
-            . $term->name . 
-          '</a>
-        </h2>'
-      );
-    }
-  ?>
+          echo (
+            '<li class="' . $term_class . '">
+                <a href="' . $term_link  . '">
+                  '. $term->name .'
+                </a>
+            </li>'
+          );
+        }
+      ?>
+    </ul>
 
   </main><!-- #main -->
 </div><!-- #primary -->
