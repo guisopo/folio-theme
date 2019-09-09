5fb6ef92d934375fc5597c3d59fc05a66771a796
guisopo <solerguillermo@hotmail.com>
2019-09-09 21:11:00 +0200

    Add basic structure for contact custom page

---
+++ page-contact.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Custom Page Templage: Contact
+ * 
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/
+ *
+ * @package Folio
+ */
+
+get_header();
+?>
+
+<?php
+  while ( have_posts() ) :
+    the_post();
+
+    get_template_part( 'template-parts/content', 'page' );
+
+  endwhile;
+?>
+
+<?php
+get_footer();
