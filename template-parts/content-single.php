73247fdf279126701a35e24d0e275684fc8fee35
guisopo <solerguillermo@hotmail.com>
2019-09-10 16:27:33 +0200

    Add links to post taxonomy  and add navigation links to browse between posts

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -13,6 +13,7 @@ get_header();
 <?php 
   $id = get_the_ID();
   $meta = get_post_meta( $id, '_avant_folio_work_info_key', true); 
+	$taxonomy_link = get_term_link( $meta['work_type'], 'work_type');
 ?>
 
 <div id="primary" class="content-area container">
@@ -20,10 +21,14 @@ get_header();
 
 		<article>
 			<header>
-				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				<div>
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</div>
 				<div>
 					<p>
-						<span><?php echo $meta['work_type'] ?></span>
+						<a href=<?php echo $taxonomy_link ?>>
+							<span><?php echo $meta['work_type'] ?></span>
+						</a>
 						<span><?php echo $meta['date_completed'] ?></span>
 					</p>
 				</div>
@@ -46,6 +51,19 @@ get_header();
 				?>
 			</div>
 		</article>
+  
+    <div>
+			<span>
+				<?php previous_post_link( '%link', 'Previous', TRUE, '', 'work_type' ); ?>
+			</span>
+			<span>
+				<a href=<?php echo $taxonomy_link ?>>Close</a>
+			</span>
+			<span>
+				<?php next_post_link( '%link', 'Next', TRUE, '', 'work_type' ); ?>
+			</span>
+    </div>
+
   </main><!-- #main -->
 
 </div><!-- #primary -->
