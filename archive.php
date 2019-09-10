bdddefc554a40cb33c88735a0906b89cad8372cd
guisopo <solerguillermo@hotmail.com>
2019-09-10 15:45:02 +0200

    Create archive page where to show a list of work_type posts

---
+++ archive.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Folio
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area container">
+  <main id="main" class="site-main">
+
+    <?php 
+    if ( have_posts() ) :
+
+      if( is_tax( 'work_type' ) ) :
+      ?>
+
+        <h1><?php single_term_title(); ?> <h1>
+        <ul>
+      <?php
+
+        while ( have_posts() ) :
+          the_post();
+
+          get_template_part( 'template-parts/content', get_post_type() );
+        endwhile;
+
+      ?>
+
+      <?php
+        
+      endif;
+
+    endif; 
+    ?>
+
+  </main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php
+get_footer();
+
+
