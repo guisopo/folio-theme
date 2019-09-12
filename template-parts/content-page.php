cd14b2dcca25f992249fc7e3e67fa6b18a4bf36b
guisopo <solerguillermo@hotmail.com>
2019-09-12 13:07:29 +0200

    Change page structures

---
+++ template-parts/content-page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Folio
+ */
+
+?>
+
+<article>
+
+	<header>
+
+		<h1><?php the_title(); ?></h1>
+
+	</header>
+
+	<div>
+
+    <?php the_content(); ?>
+      
+  </div>
+  
+</article>
