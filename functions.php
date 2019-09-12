cd4c13bfd28c7b42dd4443a9c8f76ee05ef71608
guisopo <solerguillermo@hotmail.com>
2019-09-12 16:53:26 +0200

    Change file name and move render_post_nav to that file

--- functions.php
+++ functions.php
@@ -10,23 +10,4 @@
 require get_template_directory().'/inc/cleanup.php';
 require get_template_directory().'/inc/enqueue.php';
 require get_template_directory().'/inc/menu-management.php';
-require get_template_directory().'/inc/folio-template-tags.php';
-
-function render_post_nav( $go_back_link, $taxonomy ) {
-
-  $previous_link = get_previous_post_link( '%link', 'Previous', TRUE, '', $taxonomy );
-  $next_link = get_next_post_link( '%link', 'Next', TRUE, '', $taxonomy );
-
-  return '
-    <div class="post-navigation">
-      <span class="previous-post">
-        '. $previous_link .'
-      </span>
-      <span class="go-back-button">
-        <a href='. $go_back_link .'>Close</a>
-      </span>
-      <span class="next-post">
-        '. $next_link .'
-      </span>
-    </div>';
-}
+require get_template_directory().'/inc/folio-template-helpers.php';
