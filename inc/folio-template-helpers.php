e75fa77a4f237b587fbb9fc676f7a91bafd4ae2b
guisopo <solerguillermo@hotmail.com>
2019-09-13 14:25:21 +0200

    Create helper functions to render custom html from metaboxes info and taxonomies

--- inc/folio-template-helpers.php
+++ inc/folio-template-helpers.php
@@ -4,7 +4,7 @@
  *
  * @see get_object_taxonomies()
  */
-function folio_return_custom_taxonomy( $custom_taxonomy) {
+function folio_return_custom_taxonomy( $custom_taxonomy ) {
 
   if ( ! $post = get_post() ) {
     return '';
@@ -32,10 +32,14 @@ function folio_return_custom_taxonomy( $custom_taxonomy) {
  * @see get_previous_post_link()
  * @see get_next_post_link()
  */
-function render_post_nav( $go_back_link, $taxonomy ) {
+function folio_render_post_nav( $taxonomy ) {
 
   $previous_link = get_previous_post_link( '%link', 'Previous', TRUE, '', $taxonomy );
   $next_link = get_next_post_link( '%link', 'Next', TRUE, '', $taxonomy );
+  
+  $tax_terms = get_the_terms('', $taxonomy);
+  $tax_name = $tax_terms[0]->name;
+  $go_back_link = get_term_link( $tax_name, $taxonomy);
 
   return '
     <div class="post-navigation">
@@ -57,7 +61,7 @@ function render_post_nav( $go_back_link, $taxonomy ) {
  * @see get_terms()
  * @see get_term_link()
  */
-function create_taxonmy_list( $tax_name ) {
+function folio_create_taxonmy_list( $tax_name ) {
 
   $terms_args = array(
     'taxonomy'   => $tax_name,
@@ -88,4 +92,57 @@ function create_taxonmy_list( $tax_name ) {
   $output .= '</ul>';
 
   return $output;
+}
+
+/**
+ * Get custom meta values from post.
+ *
+ * @see get_post_meta()
+ */
+function folio_get_post_meta( $post_id ) {
+  return get_post_meta( $post_id, '_avant_folio_work_info_key', true);
+}
+
+/**
+ * Render Work CPT Information.
+ *
+ * @see get_post_meta()
+ */
+function folio_render_work_info( $meta ) {
+  ?>
+  <p class="post-item__data">
+    <span><?php the_title(); ?></span>,&nbsp
+    <span><?php echo $meta['date_completed']; ?></span>
+  </p>
+
+  <p class="post-item__data">
+    <span><?php echo $meta['material']; ?></span>
+  </p>
+
+  <p class="post-item__data">
+    <span><?php echo $meta['dimensions']; ?></span>
+    <span><?php echo $meta['units']; ?></span>
+  </p>
+  <?php
+}
+
+/**
+ * Render Gallery Images saved in Custom Meta Box.
+ *
+ * @see get_post_meta()
+ */
+function folio_render_gallery( $gallery_array) {
+  $images_id = explode(',', $gallery_array );
+
+  $figure = '<figure class="post__gallery">';
+
+  foreach ($images_id as $image_id) {
+    $image =  wp_get_attachment_image( $image_id, 'medium', false );
+    
+    $figure .= '<div class="post__image">' . $image . '</div>';
+  }
+
+  $figure .= '</figure>';
+
+  return $figure;
 }
